6bada3af6703af9de6f7231efe2fd97f22c68ebb
azg2022 <15056560530@139.com>
2022-07-29 06:45:10 -0700

    添加登陆功能

--- views.py
+++ views.py
@@ -1,6 +1,11 @@
 from dganjo.http import HttpResponse
+from dganjo.shortcut import Redirect
+
 
 def index(request):
 
     return HttpResponse('ok')
 
+def login(request): 
+
+    return Redirect('index')
